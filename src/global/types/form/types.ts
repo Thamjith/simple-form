44f3fa1f8fc086c10608ef203fabed960ffa6e9d
Thamjith Thaha <thamjith.thaha@ibm.com>
2024-07-21 16:27:08 +0530

    updated password criteria

--- src/global/types/form/types.ts
+++ src/global/types/form/types.ts
@@ -42,7 +42,19 @@ export const UserSchema: ZodType<FormData> = z
       password: z
          .string()
          .min(8, { message: 'Password is too short' })
-         .max(20, { message: 'Password is too long' }),
+         .max(20, { message: 'Password is too long' })
+         .regex(/[A-Z]/, {
+            message: 'Password must contain at least one uppercase letter',
+         })
+         .regex(/[a-z]/, {
+            message: 'Password must contain at least one lowercase letter',
+         })
+         .regex(/[0-9]/, {
+            message: 'Password must contain at least one number',
+         })
+         .regex(/[\W_]/, {
+            message: 'Password must contain at least one special character',
+         }),
       confirmPassword: z.string(),
    })
    .refine((data) => data.password === data.confirmPassword, {
